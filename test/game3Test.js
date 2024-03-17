fcd8234ea77a3c8d9eca435c2124983d17bbcaa6
activiteOCR <julien.terrier3@wanadoo.fr>
2024-03-17 19:06:37 +0100

    Add week5 Group Activity: Contract Puzzles! 5 test passed

--- test/game3Test.js
+++ test/game3Test.js
@@ -1,35 +1,57 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game3', function () {
+describe("Game3", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game3');
+    const Game = await ethers.getContractFactory("Game3");
     const game = await Game.deploy();
 
     // Hardhat will create 10 accounts for you by default
     // you can get one of this accounts with ethers.provider.getSigner
     // and passing in the zero-based indexed of the signer you want:
     const signer = ethers.provider.getSigner(0);
+    const signer_2 = ethers.provider.getSigner(1);
+    const signer_3 = ethers.provider.getSigner(2);
 
     // you can get that signer's address via .getAddress()
     // this variable is NOT used for Contract 3, just here as an example
-    const address = await signer.getAddress();
+    const address_1 = await signer.getAddress();
+    const address_2 = await signer_2.getAddress();
+    const address_3 = await signer_3.getAddress();
 
-    return { game, signer };
+    return {
+      game,
+      signer,
+      signer_2,
+      signer_3,
+      address_1,
+      address_2,
+      address_3,
+    };
   }
 
-  it('should be a winner', async function () {
-    const { game, signer } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const {
+      game,
+      signer,
+      signer_2,
+      signer_3,
+      address_1,
+      address_2,
+      address_3,
+    } = await loadFixture(deployContractAndSetVariables);
 
     // you'll need to update the `balances` mapping to win this stage
+    await game.connect(signer_3).buy({ value: "1" });
 
     // to call a contract as a signer you can use contract.connect
-    await game.connect(signer).buy({ value: '1' });
+    await game.connect(signer).buy({ value: "2" });
+    await game.connect(signer_2).buy({ value: "3" });
 
     // TODO: win expects three arguments
-    await game.win();
+    await game.win(address_1, address_2, address_3);
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
--- test/game5Test.js
+++ test/game5Test.js
@@ -1,21 +1,32 @@
-const { loadFixture } = require('@nomicfoundation/hardhat-network-helpers');
-const { assert } = require('chai');
+const { loadFixture } = require("@nomicfoundation/hardhat-network-helpers");
+const { assert } = require("chai");
 
-describe('Game5', function () {
+describe("Game5", function () {
   async function deployContractAndSetVariables() {
-    const Game = await ethers.getContractFactory('Game5');
+    const Game = await ethers.getContractFactory("Game5");
+
     const game = await Game.deploy();
 
-    return { game };
+    const owner = await ethers.getImpersonatedSigner(
+      "0x0000000000000000000000000000000000000000"
+    );
+
+    // Set the balance of the impersonated account
+    await hre.network.provider.send("hardhat_setBalance", [
+      "0x0000000000000000000000000000000000000000", // The impersonated account's address
+      "0x3635C9ADC5DEA00000", // The balance you want to set (in wei, this example sets 1000 ETH)
+    ]);
+
+    return { game, owner };
   }
-  it('should be a winner', async function () {
-    const { game } = await loadFixture(deployContractAndSetVariables);
+  it("should be a winner", async function () {
+    const { game, owner } = await loadFixture(deployContractAndSetVariables);
 
     // good luck
 
-    await game.win();
+    await game.connect(owner).win();
 
     // leave this assertion as-is
-    assert(await game.isWon(), 'You did not win the game');
+    assert(await game.isWon(), "You did not win the game");
   });
 });
